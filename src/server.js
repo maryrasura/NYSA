bc38d3c5b269563c7a61bbb34bc7bd0e02c2d621
Abhinav Madahar <abhinavmadahar@gmail.com>
2016-09-18 10:27:06 -0400

    Server can find the danger of a location given coords

--- src/server.js
+++ src/server.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const request = require("request");
 
 const port = 8080;
 
 const app = express();
-app.use(express.static("public"));
+app.use(express.static("NYSA Website"));
+
+app.get("/danger", (req, response) => {
+	const baseURL = "https://api.scriptrapps.io/nysa/danger";
+	const auth_token = "TDBFNDUxNzg1MzpzY3JpcHRyOkFGNEMwRjJEODE3OTUyRUFBQzZCNDA2M0YzMzM2MkZD";
+	const { latitude, longitude } = req.query;
+	const uri = `${baseURL}?auth_token=${auth_token}&latitude=${latitude}&longitude=${longitude}`;
+	console.log(uri);
+	request(uri, (error, code, body) => {
+		if (error) {
+			response.send(400);
+			return;
+		}
+
+		console.log(body);
+		response.json({
+			danger: JSON.parse(body).response.result
+		});
+	})
+})
 
 app.listen(port, () => console.log("Listening to port", port));
